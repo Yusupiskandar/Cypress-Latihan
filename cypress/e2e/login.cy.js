26dd07833c07c0102f9e1ff4b35b4562cec1c851
Yusupiskandar <iskandaryusup465@gmail.com>
2025-10-08 19:22:33 +0700

    cek kode pengiriman

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -10,6 +10,15 @@ describe("Login Page Test", () => {
         cy.get('h4.header-track-shipment-title.tx-purple')
         cy.should('be.visible')
         cy.should('contain.text', 'Lacak Pengiriman')
+        // verify logo and kode pengiriman :
+        cy.get('.track-shipment-label')
+        cy.get('img[alt="paxel awb"]')
+        cy.should('be.visible')
+        cy.should('have.attr', 'src')
+        cy.should('include', 'two-colored-airway-bill.svg')
+        // verify <span>Kode Pengiriman:</span>
+        cy.contains('span', 'Kode Pengiriman')
+        cy.should('be.visible')
         //cek coloum lacak pengiriman
         cy.get('#awb-text-area')
         cy.should('be.visible')
@@ -23,6 +32,10 @@ describe("Login Page Test", () => {
         cy.should('have.class', 'disabled')
         //Klik tombol (jika tidak disabled)
         //cy.get('#track-captcha').click()
+        //verify label track shipment
+        cy.get('.track-shipment-description')
+        cy.should('be.visible')
+        cy.should('contain.text', 'Kamu bisa masukkan maksimal sampai dengan 20 Kode Pengiriman')
     })
 
 })
