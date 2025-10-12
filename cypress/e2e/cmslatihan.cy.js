a03a1b06eef9a59c51c57583c6e31304f96a567d
Yusupiskandar <iskandaryusup465@gmail.com>
2025-10-12 23:29:36 +0700

    add test case Latihan Cypress di CMS Paxel

---
+++ cypress/e2e/cmslatihan.cy.js
@@ -0,0 +1,22 @@
+describe('Verify dashboard admin panel', () => {
+  it('should load admin CMS page', () => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      if (err.message.includes('$ is not defined')) {
+        return false
+      }
+    })
+
+    cy.visit('https://dev-cms.paxel.co/')
+    cy.title().should('contain', 'CMS') // contoh verifikasi ringan
+    cy.get('.login img[alt="logo"]')
+        .should('be.visible')
+        .should('have.attr', 'src', 'img/logo.png')
+    cy.get('.logo strong')
+        .should('contains.text', 'PAXEL')
+    cy.get('.logo')
+        .should('contains.text', 'CMS')
+    
+        //.should('have.strong.text', 'PAXEL')
+    //cy.get('.gsi-material-button-contents').click()
+  })
+})
